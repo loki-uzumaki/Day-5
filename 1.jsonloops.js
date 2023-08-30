4ad508d4b2fd0156d4f7359f1b4e913be2861dc7
Lokesh K <113424966+loki-uzumaki@users.noreply.github.com>
2023-08-30 13:21:32 +0530

    Update 1.jsonloops.js

--- 1.jsonloops.js
+++ 1.jsonloops.js
@@ -1,42 +1,32 @@
-let book = {
-    name: 'Onepiece',
-    Author: 'Oda',
-    Episode: 1073,
-    Anime: 'yes',
-    story: 'pirates'
-}
+let book = '{ "name": "Onepiece", "Author": "Oda", "Episode": 1073, "Anime": "yes", "story": "pirates" }'
 
 // for loop
-array = []
-array.push(book)
-for (index = 0; index < array.length; index++) {
-    console.log(array[index])
+let jsonObject = JSON.parse(book)
+for (let index = 0; index < (Object.keys(jsonObject)).length; index++) {
+    console.log(Object.keys(jsonObject)[index], ':', jsonObject[Object.keys(jsonObject)[index]])
 }
 
 
-// for in
+// // for in
 
 for (let key in book) {
     console.log(key, book[key])
 }
 
 
-// for of
+// // for of
 
-array = []
-array.push(book)
-for (let value of array) {
-    console.log(value)
+
+let jsonObject = JSON.parse(book)
+for (let entry of Object.entries(jsonObject)) {
+    console.log(entry[0], ':', entry[1])
 }
 
 
-// for each
-
-array = []
-array.push(book)
-function print(value) {
-    console.log(value);
-}
-array.forEach(print)
+// // for each
+let jsonObject = JSON.parse(book)
+Object.entries(jsonObject).forEach((entry) => {
+    console.log(entry[0], ':', entry[1])
+})
 
 
